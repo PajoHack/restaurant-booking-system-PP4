d75af0e774b089888219914b7428a73cbcdb8da8
Patrick Hackman <pajohack@gmail.com>
2023-06-15 19:51:12 +0000

    Add class based views in bookings/views.py for Restaurant & Bookings

---
+++ bookings/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import RestaurantListView, RestaurantDetailView, RestaurantCreateView, RestaurantUpdateView, RestaurantDeleteView
+
+
+urlpatterns = [
+    path('restaurant/', RestaurantListView.as_view(), name='restaurant_list'),
+    path('restaurant/<int:pk>/', RestaurantDetailView.as_view(), name='restaurant_detail'),
+    path('restaurant/new/', RestaurantCreateView.as_view(), name='restaurant_new'),
+    path('restaurant/<int:pk>/edit/', RestaurantUpdateView.as_view(), name='restaurant_edit'),
+    path('restaurant/<int:pk>/delete/', RestaurantDeleteView.as_view(), name='restaurant_delete'),
+]
--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Restaurant, Table, Booking, MenuItem
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+# Restaurant Views
+
+class RestaurantListView(ListView):
+    model = Restaurant
+    template_name = 'restaurant_list.html'
+
+class RestaurantDetailView(DetailView):
+    model = Restaurant
+    template_name = 'restaurant_detail.html'
+
+class RestaurantCreateView(CreateView):
+    model = Restaurant
+    template_name = 'restaurant_new.html'
+    fields = ('name', 'address', 'opening_time', 'closing_time')
+
+class RestaurantUpdateView(UpdateView):
+    model = Restaurant
+    template_name = 'restaurant_edit.html'
+    fields = ('name', 'address', 'opening_time', 'closing_time')
+
+class RestaurantDeleteView(DeleteView):
+    model = Restaurant
+    template_name = 'restaurant_delete.html'
+    success_url = reverse_lazy('restaurant_list')
+
+# Booking Views
+
+class BookingListView(ListView):
+    model = Booking
+    template_name = 'booking_list.html'
+
+class BookingDetailView(DetailView):
+    model = Booking
+    template_name = 'booking_detail.html'
+
+class BookingCreateView(CreateView):
+    model = Booking
+    template_name = 'booking_new.html'
+    fields = ('user', 'restaurant', 'table', 'date', 'time', 'guests', 'status')
+
+class BookingUpdateView(UpdateView):
+    model = Booking
+    template_name = 'booking_edit.html'
+    fields = ('user', 'restaurant', 'table', 'date', 'time', 'guests', 'status')
+
+class BookingDeleteView(DeleteView):
+    model = Booking
+    template_name = 'booking_delete.html'
+    success_url = reverse_lazy('booking_list')
