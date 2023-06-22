14a1b9e16192d6733fb83ebbd4e873692fbd28e3
Patrick Hackman <pajohack@gmail.com>
2023-06-22 14:05:43 +0000

    Add link to logged in users profile page in site nav, only visible when logged in

--- static/js/timepicker_init.js
+++ static/js/timepicker_init.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-    $('.timepicker').timepicker({ 'timeFormat': 'H:i' });
+    $('.timepicker').timepicker({
+        'timeFormat': 'H:i',
+        'minTime': '12:00',
+        'maxTime': '21:30',
+        'step': 30  // increment of 30 minutes
+    });
 });
+
